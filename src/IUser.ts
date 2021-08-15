260d20991b3c8b97ab802eac20a6dc1d6319fb2d
Alysander Stanley <alysander@gmail.com>
2021-08-15 20:28:39 +1000

    Added IUser type and wrote all the user deets into an example file

---
+++ src/IUser.ts
@@ -0,0 +1,6 @@
+
+export interface IUser {
+    full_name: string,
+    avatar: string,
+}
+
---
+++ src/exampleData/example_authors.ts
@@ -0,0 +1,40 @@
+import { IUser } from "../IUser";
+
+import avatar1 from "./assets/mellisa.svg";
+import avatar2 from "./assets/david.svg";
+import avatar3 from "./assets/evans.svg";
+import avatar4 from "./assets/sara.svg";
+import avatar5 from "./assets/guy.svg";
+import avatar6 from "./assets/guy1.svg";
+
+const Mellisa: IUser = {
+    full_name: "Mellisa Bay",
+    avatar: avatar1,
+}
+
+const David: IUser = {
+    full_name: "David Rug",
+    avatar: avatar2
+}
+
+const Evans: IUser = {
+    full_name: "Evans Jerry",
+    avatar: avatar3,
+}
+const Sara: IUser = {
+    full_name: "Sara Jack",
+    avatar: avatar4,
+}
+
+//These names never show in the example
+const Max: IUser = {
+    full_name: "Max Power",
+    avatar: avatar5,
+}
+
+const Sam: IUser = {
+    full_name: "Sam Serious",
+    avatar: avatar6,
+}
+
+export { Mellisa, David, Evans, Sara, Max, Sam };
