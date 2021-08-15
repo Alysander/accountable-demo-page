44584b09b8320e55c19142626195035ab17ff901
Alysander Stanley <alysander@gmail.com>
2021-08-15 21:24:57 +1000

    Added basic article text and layout

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -17,8 +17,23 @@ const Article = ({
     reactionCount,
     timePosted }: IArticle) => (
 
-    <article className="top-level-container">
-
+    <article className="top-level-container py-3 flex flex-col gap-3">
+        <div className="flex justify-between">
+            <div className="flex">
+                <img src={author.avatar} alt="Avatar" className="rounded-full" />
+                <div className="flex flex-col ml-2">
+                    <h2 className="font-bold">
+                        {author.full_name}
+                    </h2>
+                    <p className="text-gray">
+                        {timePosted}
+                    </p>
+                </div>
+            </div>
+        </div>
+        <p>
+            {description}
+        </p>
     </article>
 )
 
