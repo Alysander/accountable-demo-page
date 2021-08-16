25ec36482e28cc6e61adc14b95c3fbbdaa9954d8
Alysander Stanley <alysander@gmail.com>
2021-08-16 22:40:53 +1000

    Add top-level-max-width for large screens

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import logo from "../assets/Logo.svg"
 
 const Header = () => (
     <header className="px-4 py-4 mb-8 bg-white md:px-8">
-        <div className="mx-auto flex justify-between">
+        <div className="flex justify-between top-level-max-width ">
 
             <img src={logo} alt="Logo" />
             <img src={hamburger} alt="Menu" />
