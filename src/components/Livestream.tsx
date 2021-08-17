f77108c0fc350785c2e64145dcb6da70a4335526
Alysander Stanley <alysander@gmail.com>
2021-08-17 16:07:51 +1000

    Make livestream square on lg:, cinematic cover md:

--- src/components/Livestream.tsx
+++ src/components/Livestream.tsx
@@ -2,8 +2,8 @@ import livestreamPreview from "../assets/livestreamPreview.jpg";
 import eye from "../assets/eye.svg"
 
 const Livestream = () => (
-    <div className="base-container relative mt-2 md:mt-unset">
-        <img src={livestreamPreview} alt="Livestream" />
+    <div className="base-container relative mt-2 md:mt-unset md:h-3/4 lg:h-full">
+        <img src={livestreamPreview} alt="Livestream" className="md:object-cover lg:h-full" />
 
         <div className="absolute top-0 left-0 right-0 bottom-0 fade-overlay-bg p-1em flex flex-col justify-between text-white text-xs" >
             <div>
